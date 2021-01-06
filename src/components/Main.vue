3ca4fb671751766ccf5b7f2fe7ef2b352cbc33c4
Matthias Büttgenbach <matthias.buettgenbach@cologne-intelligence.de>
2021-01-06 14:03:14 +0100

    new responsive design, added preview, open task in dialog

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,48 +1,51 @@
 <template>
-  <v-container
-    fluid
-    class="pa-6"
-    >
+  <v-container class="pa-auto" height="500">
     <v-row>
       <v-col
-        v-for="n in 12"
+        v-for="n in 36"
         :key="n"
-        class="d-xs-flex child-flex pa-1"
+        class="d-lg-flex child-flex pa-1"
         cols="2"
       >         
-        <v-card class="fill-height" :color="setColor(n)">
-          <v-responsive :aspect-ratio="1">
+        <v-responsive :aspect-ratio="1">
+          <v-dialog v-model="tasks[n-1].dialog" max-width="500">
+            <template v-slot:activator="{ on, attrs }">
+              <v-card class="
+                fill-height                  
+                text-lg-h5"                  
+                v-bind="attrs"
+                v-on="on"
+                elevation="4"
+                :color="setColor(n)">
 
-            <!-- Aufgabe noch offen -->
-            <v-card-subtitle v-if="tasks[n-1].src === null" class="              
-              fill-height
-              content
-              text-caption"
-              @click="sendEmail(n)"              
-            >
-              {{tasks[n-1].text}}
-            </v-card-subtitle>
-
-            <!-- Aufgabe gelöst -->
-            <v-dialog v-else v-model="tasks[n-1].dialog" max-width="750">
-              <template v-slot:activator="{ on, attrs }">
-                <v-card-subtitle class="
-                  fill-height
-                  content"
-                  v-bind="attrs"
-                  v-on="on"
-                >
+                <v-card-text v-if="!isDone(n)" class="fill-height content text-lg-h6">
+                  {{createPreview(n)}}
+                </v-card-text>
+                <v-card-title v-else class="fill-height font-weight-black content">
                   {{tasks[n-1].owner}}
-                </v-card-subtitle>
-              </template>
-              <v-img            
-                :src="tasks[n-1].src"
-                aspect-ratio="1.67"
-              >
-              </v-img>
-            </v-dialog>
-          </v-responsive>
-        </v-card>           
+                </v-card-title>
+              </v-card>
+            </template>              
+            <v-card color="amber lighten-5">
+              <v-img v-if="isDone(n)" :src="tasks[n-1].src" />
+              <v-card-text v-else class="pa-5">
+                <h3 class="content">
+                  {{tasks[n-1].text}}
+                </h3>
+              </v-card-text>
+              <v-divider/>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="primary" text @click="tasks[n-1].dialog = false">
+                  Zurück
+                </v-btn>
+                <v-btn v-show="!isDone(n)" color="primary" text @click="sendEmail(n)">
+                  Einsenden
+                </v-btn>
+              </v-card-actions>
+            </v-card>              
+          </v-dialog>
+        </v-responsive>
       </v-col>
     </v-row>
   </v-container>
@@ -65,7 +68,90 @@ export default {
       {
         text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, non?',
         src: 'img/pexels-chokniti-khongchum-2280571.jpg',
-        owner: 'Chokniti Khongchum',
+        owner: 'CK',
+        color: '#CCDD12',
+        dialog: false
+      },
+      {
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, non?',
+        src: null,
+        owner: 'FB',
+        color: '#0000FF'
+      },
+      {
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, non?',
+        src: 'img/pexels-stein-egil-liland-3408744.jpg',
+        owner: 'EL',
+        color: '#FEEF44',
+        dialog: false
+      },
+      {
+        text: '123456789 123456789 123456789 123456789 123456789',
+        src: null,
+        owner: 'Foo Bar',
+        color: '#BCAAA4',
+        dialog: false
+      },
+      {
+        text: 'Lorem ipsum dolor sit amet.',
+        src: null,
+        owner: 'Foo Bar',
+        color: '#BCAAA4',
+        dialog: false
+      },
+      {
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, non?',
+        src: null,
+        owner: 'Foo Bar',
+        color: '#BCAAA4',
+        dialog: false
+      },
+      {
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, non?',
+        src: null,
+        owner: 'Foo Bar',
+        color: '#BCAAA4',
+        dialog: false
+      },
+      {
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, non?',
+        src: null,
+        owner: 'Foo Bar',
+        color: '#BCAAA4',
+        dialog: false
+      },
+      {
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, non?',
+        src: null,
+        owner: 'Foo Bar',
+        color: '#BCAAA4',
+        dialog: false
+      },
+      {
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, non?',
+        src: null,
+        owner: 'Foo Bar',
+        color: '#CCCCCC',
+        dialog: false
+      },
+      {
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, non?',
+        src: null,
+        owner: 'Foo Bar',
+        color: '#CCDD12',
+        dialog: false
+      },
+      {
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, non?',
+        src: 'img/pexels-paul-ijsendoorn-33041.jpg',
+        owner: 'PI',
+        color: '#12AB34',
+        dialog: false
+      },
+            {
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, non?',
+        src: 'img/pexels-chokniti-khongchum-2280571.jpg',
+        owner: 'CK',
         color: '#CCDD12',
         dialog: false
       },
@@ -78,76 +164,178 @@ export default {
       {
         text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, non?',
         src: 'img/pexels-stein-egil-liland-3408744.jpg',
-        owner: 'Egil Liland',
+        owner: 'EL',
         color: '#FEEF44',
         dialog: false
       },
       {
-        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, non?',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, at quidem fuga id distinctio omnis?',
         src: null,
         owner: 'Foo Bar',
-        color: '#BCAAA4'
+        color: '#BCAAA4',
+        dialog: false
       },
       {
         text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, non?',
         src: null,
         owner: 'Foo Bar',
-        color: '#BCAAA4'
+        color: '#BCAAA4',
+        dialog: false
       },
       {
         text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, non?',
         src: null,
         owner: 'Foo Bar',
-        color: '#BCAAA4'
+        color: '#BCAAA4',
+        dialog: false
+      },
+      {
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, at quidem fuga id distinctio omnis?',
+        src: null,
+        owner: 'Foo Bar',
+        color: '#BCAAA4',
+        dialog: false
       },
       {
         text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, non?',
         src: null,
         owner: 'Foo Bar',
-        color: '#BCAAA4'
+        color: '#BCAAA4',
+        dialog: false
       },
       {
         text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, non?',
         src: null,
         owner: 'Foo Bar',
-        color: '#BCAAA4'
+        color: '#BCAAA4',
+        dialog: false
       },
       {
         text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, non?',
         src: null,
         owner: 'Foo Bar',
-        color: '#BCAAA4'
+        color: '#CCCCCC',
+        dialog: false
       },
       {
         text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, non?',
         src: null,
         owner: 'Foo Bar',
-        color: '#CCCCCC'
+        color: '#CCDD12',
+        dialog: false
+      },
+      {
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, at quidem fuga id distinctio omnis?',
+        src: 'img/pexels-paul-ijsendoorn-33041.jpg',
+        owner: 'PI',
+        color: '#12AB34',
+        dialog: false
       },
       {
         text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, non?',
         src: null,
         owner: 'Foo Bar',
-        color: '#CCDD12'
+        color: '#BCAAA4',
+        dialog: false
+      },
+      {
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, non?',
+        src: null,
+        owner: 'Foo Bar',
+        color: '#BCAAA4',
+        dialog: false
+      },
+      {
+        text: 'Lorem ipsum dolor sit amet.',
+        src: null,
+        owner: 'Foo Bar',
+        color: '#BCAAA4',
+        dialog: false
+      },
+      {
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, non?',
+        src: null,
+        owner: 'Foo Bar',
+        color: '#BCAAA4',
+        dialog: false
+      },
+      {
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, non?',
+        src: null,
+        owner: 'Foo Bar',
+        color: '#BCAAA4',
+        dialog: false
+      },
+      {
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, non?',
+        src: null,
+        owner: 'Foo Bar',
+        color: '#CCCCCC',
+        dialog: false
+      },
+      {
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, non?',
+        src: null,
+        owner: 'Foo Bar',
+        color: '#CCDD12',
+        dialog: false
       },
       {
         text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, non?',
         src: 'img/pexels-paul-ijsendoorn-33041.jpg',
-        owner: 'Paul Ijsendoorn',
+        owner: 'PI',
         color: '#12AB34',
         dialog: false
       },
+      {
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, non?',
+        src: null,
+        owner: 'Foo Bar',
+        color: '#BCAAA4',
+        dialog: false
+      },
+      {
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, non?',
+        src: null,
+        owner: 'Foo Bar',
+        color: '#BCAAA4',
+        dialog: false
+      },
+      {
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, non?',
+        src: null,
+        owner: 'Foo Bar',
+        color: '#BCAAA4',
+        dialog: false
+      },
+      {
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, non?',
+        src: null,
+        owner: 'Foo Bar',
+        color: '#BCAAA4',
+        dialog: false
+      }
     ]
   }),
   methods: {
+    isDone(n) {
+      return this.tasks[n-1].src !== null
+    },
     sendEmail(n) {
+      this.tasks[n-1].dialog = false
       window.open('mailto:'+this.email+'?subject='+n)    
     },
-    showImage(n) {
-      alert(`Show Image ${n}`)
-    },
     setColor(n) {
-      return (this.tasks[n-1].src === null) ? 'blue lighten-1' : this.tasks[n-1].color
+      return (this.tasks[n-1].src === null) ? 'grey lighten-1' : this.tasks[n-1].color
+    },
+    createPreview(n) {
+      switch (this.$vuetify.breakpoint.name) {
+        case 'xs': return "🤔"
+        case 'sm': return this.tasks[n-1].text.substring(0, 22) + "..."
+        case 'md': return this.tasks[n-1].text.substring(0, 32) + "..."
+        case 'lg': return this.tasks[n-1].text.substring(0, 47) + "..."
+      }        
+      return this.tasks[n-1].text
     }
   },
 }
